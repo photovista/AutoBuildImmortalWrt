056136256cda68449eaf25daba1375cdb4ff67fa
photovista <96920553+photovista@users.noreply.github.com>
2025-12-24 14:43:54 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -72,6 +72,7 @@ PACKAGES="$PACKAGES luci-i18n-autoreboot-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-acme-zh-cn"
+PACKAGES="$PACKAGES acme-acmesh-dnsapi"
 PACKAGES="$PACKAGES luci-ssl-openssl"
 
 # 文件管理器
